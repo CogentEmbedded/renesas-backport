00fb83ed985388600983eb802b8f27d95b4bb94f
Magnus Damm <damm@opensource.se>
2011-04-21 22:08:46 +0900

    serial: sh-sci: suspend/resume wakeup support V2
    This patch adds wakeup support to the sh-sci driver. The serial
    core deals with all details but defaults to wakeup disabled. So
    to make use of this feature enable wakeup in sysfs:
    
    echo enabled > /sys/class/tty/ttySC0/power/wakeup
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    (cherry picked from commit 7b7b58a9417f4f33acd420cfd3d6631bddca6b12)
    
    Signed-off-by: Nguyen Hong Ky <bimtocngoan@gmail.com>

--- drivers/tty/serial/sh-sci.c
+++ drivers/tty/serial/sh-sci.c
@@ -1759,7 +1759,7 @@ static int __devinit sci_init_single(struct platform_device *dev,
 	 *
 	 * For the muxed case there's nothing more to do.
 	 */
-	port->irq		= p->irqs[SCIx_TXI_IRQ];
+	port->irq		= p->irqs[SCIx_RXI_IRQ];
 
 #ifdef CONFIG_SERIAL_SH_SCI_DMA
 	if (p->dma_dev)
