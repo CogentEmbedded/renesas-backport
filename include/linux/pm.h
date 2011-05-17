7e63368a7f9da629a8ae55cc4532e25d2b077a72
Rafael J. Wysocki <rjw@sisk.pl>
2011-05-17 23:26:00 +0200

    PM: Allow drivers to allocate memory from .prepare() callbacks safely
    If device drivers allocate substantial amounts of memory (above 1 MB)
    in their hibernate .freeze() callbacks (or in their legacy suspend
    callbcks during hibernation), the subsequent creation of hibernate
    image may fail due to the lack of memory.  This is the case, because
    the drivers' .freeze() callbacks are executed after the hibernate
    memory preallocation has been carried out and the preallocated amount
    of memory may be too small to cover the new driver allocations.
    Unfortunately, the drivers' .prepare() callbacks also are executed
    after the hibernate memory preallocation has completed, so they are
    not suitable for allocating additional memory either.  Thus the only
    way a driver can safely allocate memory during hibernation is to use
    a hibernate/suspend notifier.  However, the notifiers are called
    before the freezing of user space and the drivers wanting to use them
    for allocating additional memory may not know how much memory needs
    to be allocated at that point.
    
    To let device drivers overcome this difficulty rework the hibernation
    sequence so that the memory preallocation is carried out after the
    drivers' .prepare() callbacks have been executed, so that the
    .prepare() callbacks can be used for allocating additional memory
    to be used by the drivers' .freeze() callbacks.  Update documentation
    to match the new behavior of the code.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    (cherry picked from commit 91e7c75ba93c48a82670d630b9daac92ff70095d)
    
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -534,11 +534,15 @@ extern void device_pm_lock(void);
 extern int sysdev_resume(void);
 extern void dpm_resume_noirq(pm_message_t state);
 extern void dpm_resume_end(pm_message_t state);
+extern void dpm_resume(pm_message_t state);
+extern void dpm_complete(pm_message_t state);
 
 extern void device_pm_unlock(void);
 extern int sysdev_suspend(pm_message_t state);
 extern int dpm_suspend_noirq(pm_message_t state);
 extern int dpm_suspend_start(pm_message_t state);
+extern int dpm_suspend(pm_message_t state);
+extern int dpm_prepare(pm_message_t state);
 
 extern void __suspend_report_result(const char *function, void *fn, int ret);
 
