69d46494eb8478654c8fa85f1ae80c48e1c63626
Takahiro Miwa <takahiro.miwa.gx@renesas.com>
2014-04-10 16:52:56 +0900

    ARM: shmobile: r8a7791: Add SGX support
    Add I/O resource data, platform_device, clockdev_dev_id
    and pm_runtime information for SGX.
    
    Signed-off-by: Takahiro Miwa <takahiro.miwa.gx@renesas.com>
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>
    (cherry picked from commit 89ec71db005af9379996e68f7cfd0d8cef864f82)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    Conflicts:
    	arch/arm/mach-shmobile/Makefile
    	arch/arm/mach-shmobile/clock-r8a7791.c
    	arch/arm/mach-shmobile/pm-r8a7791.c
    	arch/arm/mach-shmobile/r8a7791.h
    	arch/arm/mach-shmobile/setup-r8a7791.c

--- arch/arm/mach-shmobile/pm-r8a7791.c
+++ arch/arm/mach-shmobile/pm-r8a7791.c
@@ -1,7 +1,7 @@
 /*
  * r8a7791 Power management support
  *
- * Copyright (C) 2014  Renesas Electronics Corporation
+ * Copyright (C) 2013-2014  Renesas Electronics Corporation
  * Copyright (C) 2011  Renesas Solutions Corp.
  * Copyright (C) 2011  Magnus Damm
  *
@@ -11,6 +11,8 @@
  */
 
 #include <linux/kernel.h>
+#include <linux/pm.h>
+#include <linux/pm_clock.h>
 #include <linux/smp.h>
 #include <asm/io.h>
 #include "common.h"
@@ -26,7 +28,7 @@
 #define SYSCIER 0x0c
 #define SYSCIMR 0x10
 
-#if defined(CONFIG_SMP)
+#if defined(CONFIG_PM) || defined(CONFIG_SMP)
 
 static void __init r8a7791_sysc_init(void)
 {
@@ -37,11 +39,11 @@ static void __init r8a7791_sysc_init(void)
 	iowrite32(0, base + SYSCIMR);
 }
 
-#else /* CONFIG_SMP */
+#else /* CONFIG_PM || CONFIG_SMP */
 
 static inline void r8a7791_sysc_init(void) {}
 
-#endif /* CONFIG_SMP */
+#endif /* CONFIG_PM || CONFIG_SMP */
 
 void __init r8a7791_pm_init(void)
 {
@@ -71,3 +73,76 @@ void __init r8a7791_pm_init(void)
 	r8a7791_sysc_init();
 	shmobile_smp_apmu_suspend_init();
 }
+
+#ifdef CONFIG_PM
+
+static int pd_power_down(struct generic_pm_domain *genpd)
+{
+	struct rcar_sysc_ch *r8a7791_ch = to_r8a7791_ch(genpd);
+	int ret;
+
+	ret =  rcar_sysc_power_down(to_r8a7791_ch(genpd));
+
+	if (r8a7791_ch->chan_offs == 0xc0) {
+		/*
+		 * Issue software reset to 3DG functional blocks right after
+		 * the SGX power shut-off to avoid a hardware lock-up issue
+		 * triggered when we bring the SGX power up next time.
+		 */
+		r8a7791_module_reset(1, BIT(12), 2); /* DVFS */
+		r8a7791_module_reset(8, BIT(0), 2); /* CONST */
+	}
+
+	return ret;
+}
+
+static int pd_power_up(struct generic_pm_domain *genpd)
+{
+	return rcar_sysc_power_up(to_r8a7791_ch(genpd));
+}
+
+static bool pd_is_off(struct generic_pm_domain *genpd)
+{
+	return rcar_sysc_power_is_off(to_r8a7791_ch(genpd));
+}
+
+static bool pd_active_wakeup(struct device *dev)
+{
+	return true;
+}
+
+static void r8a7791_init_pm_domain(struct r8a7791_pm_domain *r8a7791_pd)
+{
+	struct generic_pm_domain *genpd = &r8a7791_pd->genpd;
+
+	pm_genpd_init(genpd, NULL, true);
+	genpd->dev_ops.stop = pm_clk_suspend;
+	genpd->dev_ops.start = pm_clk_resume;
+	genpd->dev_ops.active_wakeup = pd_active_wakeup;
+	genpd->dev_irq_safe = true;
+	genpd->power_off = pd_power_down;
+	genpd->power_on = pd_power_up;
+
+	if (pd_is_off(&r8a7791_pd->genpd))
+		pd_power_up(&r8a7791_pd->genpd);
+}
+
+static struct r8a7791_pm_domain r8a7791_pm_domains[] = {
+	{
+		.genpd.name = "pvrsrvkm",
+		.ch = {
+			.chan_offs = 0xc0, /* PWRSR2 .. PWRER2 */
+			.isr_bit = 20, /* SGX */
+		},
+	},
+};
+
+void __init r8a7791_init_pm_domains(void)
+{
+	int j;
+
+	for (j = 0; j < ARRAY_SIZE(r8a7791_pm_domains); j++)
+		r8a7791_init_pm_domain(&r8a7791_pm_domains[j]);
+}
+
+#endif /* CONFIG_PM */
