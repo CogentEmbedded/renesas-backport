023f27f71d6dfccea53ded6070cacf30b7a645d0
Shinobu Uehara <shinobu.uehara.xc@renesas.com>
2014-06-25 20:35:11 +0900

    ARM: shmobile: koelsche-reference: Remove SDHI DMA slave IDs
    This patch moves the slave IDs of SYS-DMAC to the slave IDs list of
    Audio-DMAC and Audio-DMAC-pp, because the slave IDs of Audio-DMAC,
    Audio-DMAC-pp and SYS-DMAC should be unique number.
    
    Signed-off-by: Shinobu Uehara <shinobu.uehara.xc@renesas.com>
    (cherry picked from commit ab1ee50cd84ea047c575f04d534c69e71c223def)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    Conflicts:
    	arch/arm/mach-shmobile/board-koelsch-reference.c

--- arch/arm/mach-shmobile/board-koelsch-reference.c
+++ arch/arm/mach-shmobile/board-koelsch-reference.c
@@ -228,17 +228,6 @@ static const struct clk_name clk_names[] __initconst = {
 	{ "fdp0", NULL, NULL },
 };
 
-/* Local DMA slave IDs */
-enum {
-	RCAR_DMA_SLAVE_KOELSCH_INVALID = 0,
-	SYS_DMAC_SLAVE_SDHI0_TX = 64,
-	SYS_DMAC_SLAVE_SDHI0_RX,
-	SYS_DMAC_SLAVE_SDHI1_TX,
-	SYS_DMAC_SLAVE_SDHI1_RX,
-	SYS_DMAC_SLAVE_SDHI2_TX,
-	SYS_DMAC_SLAVE_SDHI2_RX,
-};
-
 #define DMAE_CHANNEL(a, b)			\
 {						\
 	.offset		= (a) - 0x20,		\
